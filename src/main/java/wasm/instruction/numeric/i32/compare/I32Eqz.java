b45e438ea8a1fea6b3c679132dddf719b1dd867a
Dawn <dawn@yinian.tech>
2021-07-22 23:45:48 +0800

    7

--- src/main/java/wasm/instruction/numeric/i32/compare/I32Eqz.java
+++ src/main/java/wasm/instruction/numeric/i32/compare/I32Eqz.java
@@ -1,5 +1,6 @@
 package wasm.instruction.numeric.i32.compare;
 
+import wasm.core.VirtualMachine;
 import wasm.core.WasmReader;
 import wasm.instruction.Operate;
 import wasm.model.Dump;
@@ -9,6 +10,11 @@ public class I32Eqz implements Operate {
     @Override
     public Dump read(WasmReader reader) {
         return null;
+    }
+
+    @Override
+    public void operate(VirtualMachine vm, Dump args) {
+        vm.pushBool(vm.popU32().parseBool());
     }
 
 }
--- src/main/java/wasm/instruction/numeric/i64/compare/I64Eqz.java
+++ src/main/java/wasm/instruction/numeric/i64/compare/I64Eqz.java
@@ -1,5 +1,6 @@
 package wasm.instruction.numeric.i64.compare;
 
+import wasm.core.VirtualMachine;
 import wasm.core.WasmReader;
 import wasm.instruction.Operate;
 import wasm.model.Dump;
@@ -9,6 +10,11 @@ public class I64Eqz implements Operate {
     @Override
     public Dump read(WasmReader reader) {
         return null;
+    }
+
+    @Override
+    public void operate(VirtualMachine vm, Dump args) {
+        vm.pushBool(vm.popU64().parseBool());
     }
 
 }
