4875207a2ca313ab6d11982a921b1c821932516c
Dawn <dawn@yinian.tech>
2021-07-23 01:31:31 +0800

    15

--- src/main/java/wasm/instruction/numeric/i64/operate/I64Add.java
+++ src/main/java/wasm/instruction/numeric/i64/operate/I64Add.java
@@ -1,14 +1,23 @@
 package wasm.instruction.numeric.i64.operate;
 
+import wasm.core.VirtualMachine;
 import wasm.core.WasmReader;
 import wasm.instruction.Operate;
 import wasm.model.Dump;
+import wasm.model.number.U64;
 
 public class I64Add implements Operate {
 
     @Override
     public Dump read(WasmReader reader) {
         return null;
+    }
+
+    @Override
+    public void operate(VirtualMachine vm, Dump args) {
+        U64 v2 = vm.popU64();
+        U64 v1 = vm.popU64();
+        vm.pushU64(v1.add(v2));
     }
 
 }
