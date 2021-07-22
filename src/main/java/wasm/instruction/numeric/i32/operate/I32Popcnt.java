61169257437e1571e11d8fdfd01c138a47d62706
Dawn <dawn@yinian.tech>
2021-07-23 00:44:43 +0800

    12

--- src/main/java/wasm/instruction/numeric/i32/operate/I32Popcnt.java
+++ src/main/java/wasm/instruction/numeric/i32/operate/I32Popcnt.java
@@ -1,14 +1,22 @@
 package wasm.instruction.numeric.i32.operate;
 
+import wasm.core.VirtualMachine;
 import wasm.core.WasmReader;
 import wasm.instruction.Operate;
 import wasm.model.Dump;
+import wasm.model.number.U32;
 
 public class I32Popcnt implements Operate {
 
     @Override
     public Dump read(WasmReader reader) {
         return null;
+    }
+
+    @Override
+    public void operate(VirtualMachine vm, Dump args) {
+        U32 v = vm.popU32();
+        vm.pushU32(v.popcnt());
     }
 
 }
--- src/main/java/wasm/instruction/numeric/i64/operate/I64Clz.java
+++ src/main/java/wasm/instruction/numeric/i64/operate/I64Clz.java
@@ -1,14 +1,22 @@
 package wasm.instruction.numeric.i64.operate;
 
+import wasm.core.VirtualMachine;
 import wasm.core.WasmReader;
 import wasm.instruction.Operate;
 import wasm.model.Dump;
+import wasm.model.number.U64;
 
 public class I64Clz implements Operate {
 
     @Override
     public Dump read(WasmReader reader) {
         return null;
+    }
+
+    @Override
+    public void operate(VirtualMachine vm, Dump args) {
+        U64 v = vm.popU64();
+        vm.pushU64(v.clz());
     }
 
 }
