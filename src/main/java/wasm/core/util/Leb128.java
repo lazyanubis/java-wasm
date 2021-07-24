d8363bab2e15365fbcb4b27ca42bee2a35278f12
Dawn <dawn@yinian.tech>
2021-07-24 11:25:23 +0800

    41

--- src/main/java/wasm/core/util/Leb128.java
+++ src/main/java/wasm/core/util/Leb128.java
@@ -1,23 +1,40 @@
-package wasm.util;
+package wasm.core.util;
 
-import wasm.model2.number.U64;
-
-import static wasm.util.NumberUtil.toBinary;
+import static wasm.core.util.NumberTransform.parseByteByBinary;
+import static wasm.core.util.NumberTransform.toBinary;
 
 public class Leb128 {
 
-    public static class U64Result {
-        public U64 result;
-        public int length;
+    /**
+     * 读取结果
+     */
+    public static class Result {
+        public final byte[] bytes;
+        public final int length;
+        private Result(byte[] bytes, int length) {
+            assert bytes.length == 8;
+
+            this.bytes = bytes;
+            this.length = length;
+        }
+
+        static Result of(String v, int length) {
+            byte[] bytes = new byte[8];
+            for (int j = 0; j < 8; j++) {
+                bytes[j] = parseByteByBinary(v.substring(j * 8, j * 8 + 8));
+            }
+            return new Result(bytes, length);
+        }
+
     }
 
-    public static class S64Result {
-        public long result;
-        public int length;
-    }
+    /**
+     * 读取无符号数字
+     */
+    public static Result decodeVarUint(byte[] data, int size) {
+        assert null != data;
+        assert size == 32 || size == 64;
 
-    public static U64Result decodeVarUint(byte[] data, int size) {
-        U64Result u64Result = new U64Result();
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < data.length; i++) {
             byte b = data[i];
@@ -39,9 +56,8 @@ public class Leb128 {
 
             if ((b & 0x80) == 0) {
                 String v = "0000000000000000000000000000000000000000000000000000000000000000" + sb;
-                u64Result.result = new U64(v.substring(v.length() - 64));
-                u64Result.length = i + 1;
-                return u64Result;
+                v = v.substring(v.length() - 64);
+                return Result.of(v, i + 1);
             }
         }
 
@@ -49,8 +65,13 @@ public class Leb128 {
     }
 
 
-    public static S64Result decodeVarInt(byte[] data, int size) {
-        S64Result s64Result = new S64Result();
+    /**
+     * 读取有符号数字
+     */
+    public static Result decodeVarInt(byte[] data, int size) {
+        assert null != data;
+        assert size == 32 || size == 64;
+
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < data.length; i++) {
             byte b = data[i];
@@ -62,9 +83,9 @@ public class Leb128 {
                     throw new RuntimeException("integer representation too long");
                 }
                 if (((b & 0x40) == 0 && b >> (size - i * 7 - 1) != 0) ||
-                        ((b & 0x40) != 0 && ((b | 0xFFFFFF80) >> (size - i * 7 - 1)) != -1)) {
-                    // 0b0100_0000 末位的第2位是符号位 如果是0则 排出需要的位 剩下的都是0
-                    // 0b0100_0000 末位的第2位是符号位 如果是1则 排出需要的位 剩下的都是必须都是1
+                    ((b & 0x40) != 0 && ((b | 0xFFFFFF80) >> (size - i * 7 - 1)) != -1)) {
+                    // 0b0100_0000 末位的第2位是符号位 如果是0 则 排出需要的位 剩下的都应该是0
+                    // 0b0100_0000 末位的第2位是符号位 如果是1 则 排出需要的位 剩下的都是必须都是1
                     throw new RuntimeException("integer too large");
                 }
             }
@@ -74,57 +95,15 @@ public class Leb128 {
 
             if ((b&0x80) == 0) {
                 String v = sb.toString();
-                if ((i*7 < size) && ((b & 0x40) != 0)) {
+                if ((i * 7 < size) && ((b & 0x40) != 0)) {
                     // 如果顶位是1 则填充满为1
                     v = "1111111111111111111111111111111111111111111111111111111111111111" + v;
-                    v = v.substring(v.length() - 64);
+                } else {
+                    v = "0000000000000000000000000000000000000000000000000000000000000000" + v;
                 }
-                s64Result.result = Long.parseUnsignedLong(v, 2);
-                s64Result.length = i + 1;
-                return s64Result;
+                v = v.substring(v.length() - 64);
+                return Result.of(v, i + 1);
             }
-
-        }
-
-        throw new RuntimeException("unexpected end of section or function");
-    }
-
-
-    public static S64Result decodeVarInt33(byte[] data) {
-        S64Result i64Result = new S64Result();
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < data.length; i++) {
-            byte b = data[i];
-
-            if (i == 33 / 7) {
-                // 如果应当是最后一个字节
-                if ((b & 0x80) != 0) {
-                    // 已经最后一个字节 第一位标识符不应该是1 如果是1表示后面还有，不应该
-                    throw new RuntimeException("integer representation too long");
-                }
-                if (((b & 0x40) == 0 && b >> (33 - i * 7 - 1) != 0) ||
-                        ((b & 0x40) != 0 && ((b | 0xFFFFFF80) >> (33 - i * 7 - 1)) != -1)) {
-                    // 0b0100_0000 末位的第2位是符号位 如果是0则 排出需要的位 剩下的都是0
-                    // 0b0100_0000 末位的第2位是符号位 如果是1则 排出需要的位 剩下的都是必须都是1
-                    throw new RuntimeException("integer too large");
-                }
-            }
-
-            // 每次只取后面7位数字
-            sb.insert(0, toBinary(b).substring(1));
-
-            if ((b & 0x80) == 0) {
-                String v = sb.toString();
-                if ((i*7 < 33) && ((b & 0x40) != 0)) {
-                    // 如果顶位是1 则填充满为1
-                    v = "1111111111111111111111111111111111111111111111111111111111111111" + v;
-                    v = v.substring(v.length() - 64);
-                }
-                i64Result.result = Long.parseUnsignedLong(v, 2);
-                i64Result.length = i + 1;
-                return i64Result;
-            }
-
         }
 
         throw new RuntimeException("unexpected end of section or function");
